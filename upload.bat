ee4dddef893aa5804223be4f308fd9f59381c143
GUO Joanna <joanna.guo@nutricia.com>
2021-10-07 14:15:26 +0800

    Add files via upload

--- upload.bat
+++ upload.bat
@@ -1,4 +1,5 @@
-
+#python setup.py sdist bdist_wheel
+#twine upload --verbose -u JamesJ -p Promotion2021 --repository pypi dist/find_primes-2.1.*
 git init -b main
 git add .
 git commit -m "Add files via upload"
