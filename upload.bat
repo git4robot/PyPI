492ac5eab73d44d3021da73a615d91b12c4b3e92
GUO Joanna <joanna.guo@nutricia.com>
2021-10-07 14:20:44 +0800

    Add files via upload

--- upload.bat
+++
@@ -1,8 +0,0 @@
-#python setup.py sdist bdist_wheel
-##twine upload --verbose -u JamesJ -p Promotion2021 --repository pypi dist/find_primes-2.1.*
-git init -b main
-git add .
-git commit -m "Add files via upload"
-git remote add origin https://github.com/git4robot/pypi_find_primes.git
-git push origin main
-pause
