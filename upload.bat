98899efd07a77b42614d43450a71255d8b5528ba
GUO Joanna <joanna.guo@nutricia.com>
2021-10-07 14:10:32 +0800

    Add files via upload

--- upload.bat
+++ upload.bat
@@ -3,5 +3,5 @@ git init -b main
 git add .
 git commit -m "Add files via upload"
 git remote add origin https://github.com/git4robot/pypi_find_primes.git
-git push origin master
+git push origin main
 pause
